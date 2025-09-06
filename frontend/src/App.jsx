73995af5647f6a4bfff72f0755c645fe8d09a180
YaserZarifi <yaserzarifi1378@gmail.com>
2025-09-06 20:53:00 +0330

    Tiny tiny change :)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -384,7 +384,7 @@ function App() {
                 rel="noopener noreferrer"
                 className="fixed bottom-4 right-4 text-sm text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-200"
             >
-                Designed by Yaser Zarifi
+                Developed by Yaser Zarifi
             </a>
         </div>
 
